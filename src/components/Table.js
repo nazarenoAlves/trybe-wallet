58dd7f40ba80a9b8f5610c4ec213fba35a8bad38
Nazareno Alves da Costa <nazarenoalvesdacosta@gmail.com>
2022-11-03 13:19:41 -0300

    requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,27 +1,54 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
       <div>
         <table>
-          <tr>
-            <th>Descrição</th>
-            <th>Tag</th>
-            <th>Método de pagamento</th>
-            <th>Valor</th>
-            <th>Moeda</th>
-            <th>Câmbio utilizado</th>
-            <th>Valor convertido</th>
-            <th>Moeda de conversão</th>
-            <th>Editar/Excluir</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((element) => (
+              <tr key={ element.id }>
+                <td>{element.description}</td>
+                <td>{element.tag}</td>
+                <td>{element.method}</td>
+                <td>{Number(element.value).toFixed(2)}</td>
+                <td>{element.exchangeRates[element.currency].name}</td>
+                <td>{Number(element.exchangeRates[element.currency].ask).toFixed(2)}</td>
+                <td>
+                  {(Number(element.value)
+                    * Number(element.exchangeRates[element.currency].ask)).toFixed(2)}
+                </td>
+                <td>Real</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     );
   }
 }
+
+Table.propTypes = {
+  expenses: PropTypes.shape({
+    map: PropTypes.func.isRequired,
+  }).isRequired,
+};
 const mapStateToProps = (globalState) => ({
   expenses: globalState.wallet.expenses,
 });
